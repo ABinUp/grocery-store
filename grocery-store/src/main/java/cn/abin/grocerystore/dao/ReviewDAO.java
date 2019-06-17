b268c18d24ee69879d81dffb51a00e2ed819b715
abin <305862051@qq.com>
2019-06-17 15:36:56 +0800

    dfd

--- grocery-store/src/main/java/cn/abin/grocerystore/dao/ReviewDAO.java
+++ grocery-store/src/main/java/cn/abin/grocerystore/dao/ReviewDAO.java
@@ -8,8 +8,16 @@ import cn.abin.grocerystore.pojo.Product;
 import cn.abin.grocerystore.pojo.Review;
 
 public interface ReviewDAO extends JpaRepository<Review,Integer>{
-	// 根据商品查询出评价
+	/**
+	 *  根据商品查询出评价
+	 * @param product
+	 * @return
+	 */
 	List<Review> findByProductOrderByIdDesc(Product product);
-	// 根据商品查出评价数，类似于group by
+	/**
+	 *  根据商品查出评价数，类似于group by
+	 * @param product
+	 * @return
+	 */
 	int countByProduct(Product product);
 }
