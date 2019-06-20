d240ddb8014512a6f9a1e8fff97533d2a323f7a3
abin <305862051@qq.com>
2019-06-20 10:23:25 +0800

    propertyValue redis

--- grocery-store/src/main/java/cn/abin/grocerystore/service/PropertyValueService.java
+++ grocery-store/src/main/java/cn/abin/grocerystore/service/PropertyValueService.java
@@ -3,6 +3,9 @@ package cn.abin.grocerystore.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import cn.abin.grocerystore.dao.PropertyValueDAO;
@@ -10,8 +13,10 @@ import cn.abin.grocerystore.pojo.Category;
 import cn.abin.grocerystore.pojo.Product;
 import cn.abin.grocerystore.pojo.Property;
 import cn.abin.grocerystore.pojo.PropertyValue;
+import cn.abin.grocerystore.util.SpringContextUtil;
 
 @Service
+@CacheConfig(cacheNames="propertyValues")
 public class PropertyValueService {
 	@Autowired
 	private PropertyValueDAO propertyValueDAO;
@@ -22,6 +27,7 @@ public class PropertyValueService {
 	 * @param p
 	 * @return
 	 */
+	@Cacheable(key="'propertyValues-'+#p0.id")
 	public List<PropertyValue> list(Product p){
 		// 查询时才检查初始化
 		init(p);
@@ -38,6 +44,7 @@ public class PropertyValueService {
 	 * @param property
 	 * @return
 	 */
+	@Cacheable(key="'propertyValues-pid-'+#p0.id+'-propertyid-'+#p1.id")
 	public PropertyValue get(Product product,Property property){
 		PropertyValue pv = propertyValueDAO.findByProductAndPropertyOrderByIdDesc(product, property);
 		// 设置属性，便于调用，获取属性名
@@ -50,20 +57,22 @@ public class PropertyValueService {
 	 *4 如果不存在，那么就创建一个属性值，并设置其属性和产品，接着插入到数据库中。
 	 *这样就完成了属性值的初始化。
 	 */
+	
 	public void init(Product product) {
+		PropertyValueService bean = SpringContextUtil.getBean(PropertyValueService.class);
 		Category category = product.getCategory();
 		List<Property> propertyList = propertyService.list(category.getId());
 		// foreach 集合为空不会影响，直接不执行而已
 		for(Property property:propertyList) {
 			// 查询唯一的属性值
-			PropertyValue pv = get(product,property);
+			PropertyValue pv = bean.get(product,property);
 			// 如果数据库中没有，则绑定商品和属性，再保存到数据库
 			if(pv==null) {
 				pv = new PropertyValue();
 				pv.setProduct(product);
 				pv.setProperty(property);
 				// 保存到数据库
-				add(pv);
+				bean.add(pv);
 			}
 		}
 	}
@@ -72,6 +81,7 @@ public class PropertyValueService {
 	 *  增加属性值字段
 	 * @param pv
 	 */
+	@CacheEvict(allEntries = true)
 	public void add(PropertyValue pv) {
 		propertyValueDAO.save(pv);
 	}
@@ -79,6 +89,7 @@ public class PropertyValueService {
 	 *  更改属性值
 	 * @param pv
 	 */
+	@CacheEvict(allEntries = true)
 	public void update(PropertyValue pv) {
 		propertyValueDAO.save(pv);
 	}
