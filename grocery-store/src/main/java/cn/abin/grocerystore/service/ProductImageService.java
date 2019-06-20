d03c384fd650451cd6b528686460ba8a3d8df30d
abin <305862051@qq.com>
2019-06-20 10:01:09 +0800

    productImage redis

--- grocery-store/src/main/java/cn/abin/grocerystore/service/ProductImageService.java
+++ grocery-store/src/main/java/cn/abin/grocerystore/service/ProductImageService.java
@@ -3,14 +3,19 @@ package cn.abin.grocerystore.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import cn.abin.grocerystore.dao.ProductImageDAO;
 import cn.abin.grocerystore.pojo.OrderItem;
 import cn.abin.grocerystore.pojo.Product;
 import cn.abin.grocerystore.pojo.ProductImage;
+import cn.abin.grocerystore.util.SpringContextUtil;
 
 @Service
+@CacheConfig(cacheNames="productImages")
 public class ProductImageService {
 	public static final String type_single = "single";
     public static final String type_detail = "detail";
@@ -25,6 +30,7 @@ public class ProductImageService {
 	 * @param product
 	 * @return
 	 */
+	@Cacheable(key="'productImagesSingle-pid'+#p0.id")
 	public List<ProductImage> listSingleProductImages(Product product){
 		return productImageDAO.findByProductAndTypeOrderByIdDesc(product, type_single);
 	}
@@ -33,6 +39,7 @@ public class ProductImageService {
 	 * @param product
 	 * @return
 	 */
+	@Cacheable(key="'productImagesDetail-pid'+#p0.id")
 	public List<ProductImage> listDetailProductImages(Product product){
 		return productImageDAO.findByProductAndTypeOrderByIdDesc(product, type_detail);
 	}
@@ -40,6 +47,7 @@ public class ProductImageService {
 	 *  增加图片，图片中封装了类型type
 	 * @param pi
 	 */
+	@CacheEvict(allEntries =true)
 	public void add(ProductImage pi) {
 		productImageDAO.save(pi);
 	}
@@ -48,6 +56,7 @@ public class ProductImageService {
 	 * @param id
 	 * @return
 	 */
+	@Cacheable(key="'productImages-one'+#p0")
 	public ProductImage get(int id) {
 		return productImageDAO.findOne(id);
 	}
@@ -56,6 +65,7 @@ public class ProductImageService {
 	 *  删除图片
 	 * @param id
 	 */
+	@CacheEvict(allEntries =true)
 	public void delete(int id) {
 		productImageDAO.delete(id);
 	}
@@ -71,7 +81,8 @@ public class ProductImageService {
 //		if(pi!=null) {
 //			product.setFirstProductImage(pi);
 //		}
-		 List<ProductImage> singleImages = listSingleProductImages(product);
+		ProductImageService bean = SpringContextUtil.getBean(ProductImageService.class);
+		 List<ProductImage> singleImages = bean.listSingleProductImages(product);
 	        if(!singleImages.isEmpty())
 	            product.setFirstProductImage(singleImages.get(0));
 	        else
